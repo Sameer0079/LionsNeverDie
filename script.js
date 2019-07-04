2dafccd5d5873c04274aae74d4896833c49a7f99
SaM <47039980+Sameer0079@users.noreply.github.com>
2019-07-04 23:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function validate(){
+  let uname=document.getElementById("uname").value;
+  let pass=document.getElementById("pass").value;
+  let result=true;
+  ((pass.length>=8) && (pass.length<15)) ? (!(uname=="sameer") || !(pass=="123456789")) ? result=false :result=true : result=false
+  return result;
+}
+
+function passShow(){
+  var a=document.getElementById("pass");
+  var b=document.getElementById("eye1");
+  var c=document.getElementById("eye2");
+  if(a.type==="password"){
+    a.type="text";
+    b.style.display="block";
+    c.style.display="none";
+  }else{
+    a.type="password";
+    b.style.display="none";
+    c.style.display="block";
+  }
+}
