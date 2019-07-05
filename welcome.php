fb178ee020d2f1f68c094a0817e66b5b0b646f29
SaM <47039980+Sameer0079@users.noreply.github.com>
2019-07-05 18:17:30 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 <body>
 
 Welcome <?php echo $_POST["n_name"]; ?><br>
-Your email address is: <?php echo $_POST["n_password"]; ?>
+Your email address is: <?php echo $_POST["pass"]; ?>
 
 </body>
 </html>
